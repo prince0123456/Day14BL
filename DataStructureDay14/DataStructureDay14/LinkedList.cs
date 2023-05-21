65329e62e6f16999c69b72a6792e7ad457bb2a82
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-21 15:15:03 +0530

    insert data in Position

---
+++ DataStructureDay14/DataStructureDay14/LinkedList.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureDay14
+{
+    public class LinkedList
+    {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} is inserted into Linkled List");
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+        public void InsertAtParticularPosition(int position, int data)
+        {
+            Node newestNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newestNode;
+            }
+            if (position == 0)
+            {
+                newestNode.next = this.head;
+                this.head = newestNode;
+                return;
+            }
+            Node previous = null;
+            Node current = this.head;
+            int count = 0;
+            while (current != null && count < position)
+            {
+                previous = current;
+                current = current.next;
+                count++;
+            }
+            newestNode.next = previous.next;
+            previous.next = newestNode;
+        }
+    }
+}
