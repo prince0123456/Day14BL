1520e1e6fe37882593f927071b976aa172c6179f
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-21 15:02:03 +0530

    Adding data reverse order

--- DataStructureDay14/DataStructureDay14/LinkedList.cs
+++ DataStructureDay14/DataStructureDay14/LinkedList.cs
@@ -6,7 +6,57 @@ using System.Threading.Tasks;
 
 namespace DataStructureDay14
 {
-    internal class LinkedList
+    public class LinkedList
     {
+        public Node head;
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine($"{node.data} is inserted into Linkled List");
+        }
+        public void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        public void Display()
+        {
+            Node temp = this.head;
+
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
     }
 }
