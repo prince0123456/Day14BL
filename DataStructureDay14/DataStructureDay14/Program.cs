c67cca16842b2fcde72e2c38b8184f6bbf20e24b
Prince Gupta <princegupta.pg46@gmail.com>
2023-05-21 14:33:44 +0530

    Creating Stack by using Node

---
+++ DataStructureDay14/DataStructureDay14/Program.cs
@@ -0,0 +1,15 @@
+﻿using DataStructureDay14;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Data Structures");
+
+        Stack stack = new Stack();
+        stack.Push(70);
+        stack.Push(30);
+        stack.Push(56);
+        stack.Display();
+    }
+}
